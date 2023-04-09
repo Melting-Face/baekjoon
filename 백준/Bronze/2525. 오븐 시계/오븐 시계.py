7027c616bed2f87c30909cfe17ac025316d887b0
Melting Face <50713792+Melting-Face@users.noreply.github.com>
2023-04-10 04:46:24 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,10 +1,13 @@
 from sys import stdin
-from datetime import datetime, timedelta
 
-start_time_txt, interval = [data.strip() for data in stdin.readlines()]
-start_time = datetime.strptime(start_time_txt, "%H %M")
-interval = timedelta(minutes=int(interval))
-end_time = start_time + interval
-hour = int(datetime.strftime(end_time, "%H"))
-minute = int(datetime.strftime(end_time, "%M"))
-print(f'{hour} {minute}')
+start_time, interval = [data.strip() for data in stdin.readlines()]
+start_hour, start_minute = [int(data) for data in start_time.split()]
+
+hour, minute = divmod(int(interval), 60)
+
+remain_hour, end_minute = divmod(start_minute + minute, 60)
+
+end_hour = start_hour + remain_hour + hour
+end_hour = int(end_hour % 24)
+
+print(end_hour, end_minute)
